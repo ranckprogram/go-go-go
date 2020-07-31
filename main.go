a0734d59b08c53c64471964c4435b2bf6d1e97d8
ranckprogram <1474574022@qq.com>
2020-07-31 18:05:04 +0800

    gin

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	// "net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/unrolled/secure"
+)
+
+func main() {
+	router := gin.New()
+	router.Use(TlsHandler())
+
+	// router.StaticFS("/static", http.Dir("/static"))
+	router.Static("/", "./static")
+
+	router.Run(":8000")
+	router.RunTLS(":8080", "ca/tls_cert.crt", "ca/tls_key.key")
+
+}
+
+func TlsHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+			secureMiddleware := secure.New(secure.Options{
+					SSLRedirect: true,
+					SSLHost:     "localhost:8080",
+			})
+			err := secureMiddleware.Process(c.Writer, c.Request)
+
+			// If there was an error, do not continue.
+			if err != nil {
+					return
+			}
+
+			c.Next()
+	}
+}
