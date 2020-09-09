3ce838b56f4c70f487d8d97728f79ff7fcf60a52
ranckprogram <1474574022@qq.com>
2020-09-09 14:18:04 +0800

    wasm 实现方法

--- README.md
+++ README.md
@@ -12,7 +12,15 @@ go get github.com/gin-gonic/gin
 
 2. 构建exe
 
-··
+```
+go build main.go
+```
+
+3. 运行
+
+```
+go run main.go
+```
 
 
 ### wasm
@@ -20,3 +28,6 @@ go get github.com/gin-gonic/gin
 ```
 GOOS=js GOARCH=wasm go build -o lib.wasm wasm.go
 ```
+
+- [ ] go中定义方法注入浏览器 window对象 中调用
+- [ ] go中调用浏览器中的方法
--- wasm.go
+++ wasm.go
@@ -1,8 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall"
+)
+
 
 func main() {
-  fmt.Println("Hello, Go WebAssembly!")
+	fmt.Println("Hello, Go WebAssembly!")
+	registerCallbacks()
+	select {}
+
+
 }
 
+func registerCallbacks() {
+	syscall.js.Global().Set("agetOne", syscall.js.FuncOf(agetOne))
+	syscall.js.Global().Set("getUserName", syscall.js.FuncOf(getUserName))
+}
+
+func agetOne(this syscall.js.Value, args []syscall.js.Value) interface{} {
+	return 2
+}
+
+func getUserName (this syscall.js.Value, args []syscall.js.Value) interface{} {
+	 username := getSystemUserName()
+	return username
+}
+
+func getSystemUserName() string {
+	var size uint32 = 128
+	var buffer = make([]uint16, size)
+	user := syscall.StringToUTF16Ptr("USERNAME")
+	domain := syscall.StringToUTF16Ptr("USERDOMAIN")
+	r, err := syscall.GetEnvironmentVariable(user, &buffer[0], size)
+	if err != nil {
+			return ""
+	}
+	buffer[r] = '@'
+	old := r + 1
+	if old >= size {
+			return syscall.UTF16ToString(buffer[:r])
+	}
+	r, err = syscall.GetEnvironmentVariable(domain, &buffer[old], size-old)
+	return syscall.UTF16ToString(buffer[:old+r])
+}
