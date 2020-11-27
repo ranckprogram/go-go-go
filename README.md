6bb0ae71e9c0aa67872526446338fb9665451eef
ranckprogram <1474574022@qq.com>
2020-11-27 09:06:01 +0800

    定义函数挂在window上

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # go-go-go
 
+学习文档
+https://golang.google.cn/pkg/
+
 ### 环境安装
 
 ### 包管理工具
@@ -37,3 +40,5 @@ GOOS=js GOARCH=wasm go build -o lib.wasm wasm.go
 
 - [ ] go中定义方法注入浏览器 window对象 中调用
 - [ ] go中调用浏览器中的方法
+
+GOOS=js GOARCH=wasm go build -o ping.wasm ping.go
--- wasm.go
+++ wasm.go
@@ -2,46 +2,29 @@ package main
 
 import (
 	"fmt"
-	"syscall"
+	"syscall/js"
 )
+
+func foo( args []js.Value) {
+	fmt.Println("hellow wasm")
+	fmt.Println(args)
+}
+
+
+var cb = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+    fmt.Println("button clicked")
+    // cb.Release() // release the function if the button will not be clicked again
+    return nil
+})
 
 
 func main() {
 	fmt.Println("Hello, Go WebAssembly!")
-	registerCallbacks()
+
+
+	js.Global().Set("aaaload", cb )
+
+  js.Global().Set("value", 100)
 	select {}
 
-
-}
-
-func registerCallbacks() {
-	syscall.js.Global().Set("agetOne", syscall.js.FuncOf(agetOne))
-	syscall.js.Global().Set("getUserName", syscall.js.FuncOf(getUserName))
-}
-
-func agetOne(this syscall.js.Value, args []syscall.js.Value) interface{} {
-	return 2
-}
-
-func getUserName (this syscall.js.Value, args []syscall.js.Value) interface{} {
-	 username := getSystemUserName()
-	return username
-}
-
-func getSystemUserName() string {
-	var size uint32 = 128
-	var buffer = make([]uint16, size)
-	user := syscall.StringToUTF16Ptr("USERNAME")
-	domain := syscall.StringToUTF16Ptr("USERDOMAIN")
-	r, err := syscall.GetEnvironmentVariable(user, &buffer[0], size)
-	if err != nil {
-			return ""
-	}
-	buffer[r] = '@'
-	old := r + 1
-	if old >= size {
-			return syscall.UTF16ToString(buffer[:r])
-	}
-	r, err = syscall.GetEnvironmentVariable(domain, &buffer[old], size-old)
-	return syscall.UTF16ToString(buffer[:old+r])
 }
